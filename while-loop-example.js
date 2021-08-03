deae34888f4af2b28746e0c7355c1eb549e0aee4
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-03 15:01:39 +0600

    while loop example added

--- while-loop-example.js
+++ while-loop-example.js
@@ -1,5 +1,19 @@
-var number =1; //loop variable
-while(number <= 10){
+//var number =1; //loop variable
+// while(number <= 20){
+//     console.log(number);
+//     number++;
+// }
+
+//odd number 
+var number = 1;
+while (number <= 20) {
     console.log(number);
-    number++;
+    number = number + 2;
+}
+
+// even numbers
+var number = 0;
+while (number <= 20) {
+    console.log(number);
+    number = number + 2;
 }
