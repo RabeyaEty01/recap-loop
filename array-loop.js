ab7507a303c996f4408fa651e5d21f9bc7b48d81
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-04 15:47:20 +0600

    array loop added

---
+++ array-loop.js
@@ -0,0 +1,11 @@
+// var numbers = [45, 77, 69, 43, 12, 87, 98, 82,90,100];
+// for(var i=0; i< numbers.length; i++){
+//     var element =numbers[i];
+//     console.log(element);
+// }
+
+var items =['bottle','mobile', 'book' ,'charger','watch','pen'];
+for(var i=0; i<items.length; i++)
+{
+    console.log(items[i]);
+}
