5547e632499615cd1fda6b208b2025ead3e269d0
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-04 13:02:30 +0600

    for loop example added

---
+++ for-loop.js
@@ -0,0 +1,28 @@
+//while loop
+//  var roastGiven = 0;
+// while(roastGiven<7){
+//          console.log('roas den , mama!!');
+//     roastGiven++;
+//  }
+
+//for loop
+ console.log('running a for loop');
+ for(var roastGiven = 0; roastGiven<7; roastGiven++){
+    console.log('Roast den, please!!');
+     console.log(roastGiven);
+ }
+//for loop with short variable
+ for (var i = 1; i <= 10; i++) {
+     console.log(i);
+}
+
+//odd
+console.log("ODD Numbers: ");
+ for (var i = 1; i <= 20; i += 2) {
+          console.log(i);
+ }
+//even
+console.log("Even Numbers: ");
+for (var i = 0; i <= 20; i += 2) {
+    console.log(i);
+}
